ab74e78fad0ca27da876776c8880c709b08e8f4d
Kanyes Thaker <krthaker1224@berkeley.edu>
2022-01-06 00:05:56 -0800

    Testing push from correct email, comment cleanup

--- src/screens/ProjectsScreen.jsx
+++ src/screens/ProjectsScreen.jsx
@@ -13,11 +13,6 @@ export default function ProjectsScreen(props) {
                         IRIS - A personal internet
                     </Link>
                 </li>
-                {/* <li className={styles.linksLi}>
-                        <a href="https://google.com">
-                            Ferret - NLP-powered auto-flashcards
-                        </a>
-                    </li> */}
                 <li className={styles.linksLi}>
                     <Link to="/ferret" calssName={styles.pageLink}>
                         Ferret - NLP-powered flashcards for the whole internet
@@ -29,26 +24,6 @@ export default function ProjectsScreen(props) {
                 <li className={styles.linksLi}>
                     <Link to="/wave" className={styles.pageLink}>Wave - Change your accent with neural voice cloning</Link>
                 </li>
-                {/* <li className={styles.linksLi}>
-                    <a href="https://drive.google.com/file/d/1IKpLYahg9lFPCBePW4mFhUHwRYzzJl3v/view">
-                        Ping - Personal CRM in React Native
-                    </a>
-                </li>
-                <li className={styles.linksLi}>
-                    <a href="https://drive.google.com/file/d/1gZLXWnhl2DsVk9K5n4Sr-c25JI5ChYrL/view">
-                        RustOS - Simple OS in Rust
-                    </a>
-                </li>
-                <li className={styles.linksLi}>
-                    <a href="https://drive.google.com/file/d/1uaw9WRfe2XzNse4c-XAv0g5B7ByV7UY0/view">
-                        Glo - LISP interpreter in Golang
-                    </a>
-                </li>
-                <li className={styles.linksLi}>
-                    <a href="https://drive.google.com/file/d/1W5DoRh0CewlhtoGpPUtNJNfiu9BHXCX3/view">
-                        ELIZA - Chatbot in Clojure
-                    </a>
-                </li> */}
                 <li className={styles.linksLi}>
                     <Link to="/drop" className={styles.pageLink}>Drop the Fax - Protest tuition hikes during COVID</Link>
                 </li>
